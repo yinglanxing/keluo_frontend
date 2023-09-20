1c5ab2a2a65470aaad479e3dffc36484090f34ce
yinglan_cat <yinglanxing@gmail.com>
2023-09-20 12:48:47 +0800

    清空store，为登录页面做准备，微调组件

---
+++ components/popup-self.vue
@@ -0,0 +1,25 @@
+<template>
+    <!--个人信息弹框-->
+    <a-popover title="Title">
+        <!--头像-->
+        <!--todo 全局登录状态-->
+        <a-avatar>
+            <icon-user/>
+        </a-avatar>
+        <!--内容-->
+        <template #content>
+
+        </template>
+    </a-popover>
+</template>
+<script lang="ts">
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
