ec38e6cbcbfe8b255d94d2fdcc442cf770314b58
yinglan_cat <yinglanxing@gmail.com>
2023-09-22 19:03:24 +0800

    结构调整,初步测试状态管理

--- components/default-header.vue
+++ components/default-header.vue
@@ -1,51 +1,43 @@
 <template>
     <!--头部-->
     <!-- 设置导航栏宽度为自动-->
-    <a-layout-header :style="{
-      width: 'auto'
-    }">
+    <a-layout-header :style="{width: 'auto'}">
         <!--TODO-->
-        <a-menu :default-selected-keys="['/']">
-            <!--          在元素之间均匀分配空间，并在首尾两端留有一半的空间-->
+        <a-menu :default-selected-keys="['1']">
+            <!--在元素之间均匀分配空间，并在首尾两端留有一半的空间-->
             <n-space justify="space-around">
 
                 <!--使用左边菜单当导航栏-->
                 <n-space>
 
                     <!--项目logo-->
-                    <!--<a-avatar :style="{ backgroundColor: '#09f' }">科洛</a-avatar>-->
                     <a-link>
                         <a-image src="/favicon.ico"></a-image>
                     </a-link>
 
-                    <a-menu-item key="/">
-                        <!--                        {{ "首页" }}-->
-                        <nuxt-link to="/">首页</nuxt-link>
+                    <a-menu-item key="1">
+                        <nuxt-link>首页</nuxt-link>
                     </a-menu-item>
 
-                    <a-menu-item key="/talks">
-                        <!--                        {{ "话题" }}-->
-                        <nuxt-link to="/talks">话题</nuxt-link>
+                    <a-menu-item>
+                        <nuxt-link>话题</nuxt-link>
                     </a-menu-item>
 
-                    <a-menu-item key="3">
-                        <!--                        {{ "文章" }}-->
-                        <nuxt-link to="/">文章</nuxt-link>
+                    <a-menu-item>
+                        <nuxt-link>文章</nuxt-link>
                     </a-menu-item>
 
-                    <a-menu-item key="4">
-                        <!--                        {{ "问答" }}-->
-                        <nuxt-link to="/">问答</nuxt-link>
+                    <a-menu-item>
+                        <nuxt-link>问答</nuxt-link>
                     </a-menu-item>
 
-                    <a-menu-item key="5">
-                        <!--                      {{ "组件商城" }}-->
-                        <nuxt-link to="/">硬件商城</nuxt-link>
+                    <a-menu-item>
+                        <nuxt-link>硬件商城</nuxt-link>
                     </a-menu-item>
 
-                    <a-menu-item key="6">
-                        <!--                      {{ "文档" }}-->
-                        <nuxt-link to="/">文档</nuxt-link>
+                    <a-menu-item>
+                        <nuxt-link>{{ "文档" }}</nuxt-link>
+                        <!--<nuxt-link>{{ $t("文档") }}</nuxt-link>-->
                     </a-menu-item>
 
                 </n-space>
@@ -63,17 +55,10 @@
                     <!--用户登录-->
                     <popup-login></popup-login>
 
-                    <!--<a-button type="primary">-->
-                    <!--    {{ "创作中心" }}-->
-                    <!--</a-button>-->
-
                     <!--发布按钮 鼠标靠近时展开一个下拉菜单-->
-                    <!--咋看咋歪，向下移动3个像素吧-->
                     <n-button-group size="large">
-                        <!--<n-button-group  style="top : 3px">-->
                         <!--左边按钮-->
                         <n-button color="#1e80ff">
-                            <!--<n-button   size="medium" type="info">-->
                             <template #icon>
                                 <icon-edit/>
                             </template>
@@ -85,7 +70,6 @@
 
                             <template #trigger>
                                 <n-button color="#1e80ff" style="width:8px">
-                                    <!--<n-button  size="medium" type="info">-->
                                     <template #icon>
                                         <icon-caret-down style="font-size: 14px"/>
                                     </template>
@@ -107,12 +91,11 @@
                     <!--主题切换-->
                     <n-button secondary size="large" @click="change_theme()" circle>
                         <template #icon>
-                            <icon-sun v-show="!theme_style"/>
-                            <icon-moon v-show="theme_style"/>
+                            <icon-sun v-show="theme == 'light'"/>
+                            <icon-moon v-show="theme != 'light'"/>
                         </template>
                     </n-button>
                 </n-space>
-
             </n-space>
 
         </a-menu>
@@ -120,35 +103,21 @@
 </template>
 
 <script lang="ts">
+
 export default {
-
     setup() {
-        // TODO setup
-    },
+        // 获取主题
+        const theme = GLOBAL_THEME()
 
-    data() {
         return {
-            // 主题 flag
-            theme_style: false,
+            theme,
+            // 切换主题
+            change_theme() {
+                let flag = theme.value == "light"
+                theme.value = flag ? "dark" : "light"
+            },
         }
     },
-    methods: {
-        // 切换主题
-        change_theme() {
-            // 切换 flag
-            this.theme_style = !this.theme_style
-        },
-        // TODO 点击跳转路由
-        chang_route(key: string) {
-            // Message.info({content: `select ${key}`, showIcon: true});
-        },
-    },
-    // 监听
-    watch: {
-        // TODO 切换主题flag时改动组件的配色
-        theme_style() {
-        },
-    }
 }
 </script>
 <style>
--- components/popup-login.vue
+++ components/popup-login.vue
@@ -13,37 +13,8 @@
         </n-space>
         <!--内容-->
         <!-- Todo 该grid卡片后续将挪动到首页上显示，单独封装成一个组件-->
-        <!-- Todo 未登录时将隐藏头像只显示登录注册按钮（单个）-->
         <template #content>
-            <a-grid :cols="{ xs: 1, lg: 2, xxl: 3 }" :colGap="24" :rowGap="24">
-                <a-grid-item :span="3">
-                    登录开启更多功能
-                </a-grid-item>
-
-                <a-grid-item>
-                    <icon-check-circle-fill/>
-                    发布你的文章
-                </a-grid-item>
-                <a-grid-item>
-                    <icon-check-circle-fill/>
-                    关注更多频道
-                </a-grid-item>
-                <a-grid-item>
-                    <icon-check-circle-fill/>
-                    敬请期待
-                </a-grid-item>
-
-                <a-grid-item :span="3">
-                    <n-button secondary type="info" block @click="alert_plane_func"> 登录</n-button>
-                </a-grid-item>
-
-                <a-grid-item :span="3">
-                    没有账号？
-                    <a-link @click="alert_plane_func">
-                        前往注册
-                    </a-link>
-                </a-grid-item>
-            </a-grid>
+            <login-tips></login-tips>
         </template>
     </a-popover>
 
